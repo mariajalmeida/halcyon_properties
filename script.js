4b8103af0c666040c5e2844c82ae38bd22d1789a
mariajalmeida <mari9d91@stud.kea.dk>
2020-10-24 20:17:09 +0200

    filters up and running

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function handleData(data) {
     const myData = data;
     console.log("myData - console");
     console.log(myData);
+    myData.reverse();
     myData.forEach(showData);
 }
 
@@ -21,6 +22,32 @@ function showData(singleRowData) {
 
     const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
+
+    // filtration i
+    const article = clone.querySelector("article");
+
+    if (singleRowData.categories[0] === 3) {
+        article.classList.add("popular");
+    }
+
+    if (singleRowData.categories[0] === 2) {
+        article.classList.add("newest");
+    }
+
+    // popular ii
+    if (singleRowData.categories[0] === 3) {
+        console.log("this is popular");
+        clone.querySelector(".popular").classList.remove("hidden");
+
+    }
+
+    // new ii
+
+    if (singleRowData.categories[0] === 2) {
+        console.log("this is new");
+        clone.querySelector(".newest").classList.remove("hidden");
+
+    }
 
     const h2 = clone.querySelector("h2");
     h2.textContent = singleRowData.title.rendered;
@@ -34,6 +61,35 @@ function showData(singleRowData) {
     const img_url = singleRowData._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
     clone.querySelector("img").src = img_url;
 
-
     document.querySelector("main").appendChild(clone);
+}
+
+
+// newest filter
+const newestFilter = document.querySelector("#newest");
+newestFilter.addEventListener("click", showNewest);
+
+function showNewest() {
+    const article = document.querySelectorAll("article:not(.newest)");
+
+    article.forEach(elem => {
+        console.log("hey there");
+        elem.classList.toggle("hidden");
+    })
+
+}
+
+
+// popular filter
+const populartFilter = document.querySelector("#popular");
+populartFilter.addEventListener("click", showPopular);
+
+function showPopular() {
+    const article = document.querySelectorAll("article:not(.popular)");
+
+    article.forEach(elem => {
+        console.log("popz in the house");
+        elem.classList.toggle("hidden");
+    })
+
 }
